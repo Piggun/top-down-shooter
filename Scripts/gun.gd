6f0a04af59d46a123a3f390799cddd3f459f8db0
Riccardo Foti <riccardofoti97@gmail.com>
2024-04-06 12:42:20 +0100

    improve reload mechanic

--- Scripts/gun.gd
+++ Scripts/gun.gd
@@ -16,12 +16,14 @@ var can_fire = true
 var fire_rate = 0.3
 var ammos = 7
 var reload_time = 1
+var is_reloading = false
 
 
 func _ready():
 	remove_child(shooting_sound)
 	get_parent().add_child(shooting_sound)
-	
+	%ReloadTimer.wait_time = reload_time
+
 	# Controller deadzone
 	InputMap.action_set_deadzone("aim_up", deadzone)
 	InputMap.action_set_deadzone("aim_down", deadzone)
@@ -37,20 +39,18 @@ func _process(delta):
 
 	
 func _physics_process(delta):
-	if Input.is_action_pressed("shoot") and can_fire and ammos > 0:
+	if Input.is_action_pressed("shoot") and can_fire and ammos > 0 and not is_reloading:
 		anim.play("shoot")
 		shoot()
 		can_fire = false
+		print(ammos)
 		await get_tree().create_timer(fire_rate).timeout
 		can_fire = true
 		ammos -= 1
-		print(ammos)
-	if Input.is_action_pressed("reload"):
-		can_fire = false
-		await get_tree().create_timer(reload_time).timeout
-		ammos = 7
-		can_fire = true
-	
+		
+	if Input.is_action_pressed("reload") and not is_reloading:
+		reload()
+	#
 	 ##Controller aim	
 	#var look_vector = Vector2(0, 0)
 	#look_vector.x = Input.get_action_strength("aim_right") - Input.get_action_strength("aim_left")
@@ -71,3 +71,18 @@ func shoot():
 	new_bullet.global_rotation = %ShootingPoint.global_rotation
 	# add bullet to parent node to avoid bullet disappearing when removing weapon
 	get_parent().add_child(new_bullet)
+	
+func reload():
+	print("reloading..")
+	is_reloading = true
+	can_fire = false
+	%ReloadTimer.start()
+		
+
+
+func _on_reload_timer_timeout():
+		ammos = 7
+		is_reloading = false
+		can_fire = true
+		%ReloadTimer.stop()
+		print("reloaded!")
